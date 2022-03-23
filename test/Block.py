f721b810522c6fb551679a79719999e74db8f0be
yuanguang-wang <yuanguang.m.wang@outlook.com>
2022-03-24 01:33:23 +0800

    T.20220324.0133

--- test/Block.py
+++ test/Block.py
@@ -4,17 +4,35 @@ import System
 import rhinoscriptsyntax as rs
 
 def test_command():
+    
     go = Rhino.Input.Custom.GetObject()
     go.SetCommandPrompt("test")
+    go.GeometryFilter = Rhino.DocObjects.ObjectType.InstanceReference
     go.GetMultiple(1, 0)
     objref = go.Objects()
     geo = []
-    tran = []
+    idefs= []
     for i in range(len(objref)):
-        geo.append(objref[i].Geometry())
-        #print(type(geo[i]))
-        tran.append(Rhino.Geometry.Brep.TryConvertBrep(geo[i]))
-        print(type(tran[i]))
+        geo.append(objref[i].Object())
+        print(type(geo[i]))
+        idef = geo[i].InstanceDefinition
+        if idef not in idefs:
+            idefs.append(idef)
+            
+    geo = []
+    for i in range(len(idefs)):
+        geo.append(idefs[i].GetObjects())
+        print(idefs[i].Index)
+        breps = []
+        #attrs = []
+        for j in range(len(geo[i])):
+            brep = geo[i][j].Geometry.DuplicateBrep()
+            brep.Faces[1].PerFaceColor = System.Drawing.Color.Red
+            #attrs.append(brep.Attributes)
+            breps.append(brep)
+        #Rhino.DocObjects.Tables.InstanceDefinitionTable.ModifyGeometry(idefs[i].Index, breps)
+        sc.doc.InstanceDefinitions.ModifyGeometry(idefs[i].Index, breps)
+
     """
     for i in range(len(objref)):
         brep = tran[i].DuplicateBrep()
